a5544f122351e1d3de4b36090a23c1cc17df6944
Kal Schumacher <schumacher.kal24@gmail.com>
2019-04-29 11:14:49 -0400

    implement all API 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
