84dba3b939b7eac8e2754bbfe6aa37539a5d219b
Kal Schumacher <schumacher.kal24@gmail.com>
2019-04-29 11:22:18 -0400

    last one

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var CLASS_COLLECTION = "attendance";
 var app = express();
 app.use(bodyParser.json());
 
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
@@ -106,3 +109,5 @@ function handleError(res, reason, message, code) {
      }
    });
  });
+
+
